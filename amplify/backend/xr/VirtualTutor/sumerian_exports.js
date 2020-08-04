a3c7410382b4a3ac63e7d2161c765cc527dadad0
EC2 Default User <ec2-user@ip-172-31-85-95.ec2.internal>
2020-08-04 20:38:00 +0000

    Production

--- amplify/backend/xr/VirtualTutor/sumerian_exports.js
+++ amplify/backend/xr/VirtualTutor/sumerian_exports.js
@@ -1,5 +1,5 @@
 {
-  "url": "https://sumerian.us-east-1.amazonaws.com/20180801/projects/Drafts-assumed-role%2FCrossAccountSignin%2Fcyrus/release/authTokens?sceneId=6ee1bb051bfe4cd59c39a22f4affd16c.scene",
-  "sceneId": "6ee1bb051bfe4cd59c39a22f4affd16c.scene",
+  "url": "https://sumerian.us-east-1.amazonaws.com/20180801/projects/Drafts-user%2Fsprank/release/authTokens?sceneId=5ba33aecba834ad2b4a104ef0e668d15.scene",
+  "sceneId": "5ba33aecba834ad2b4a104ef0e668d15.scene",
   "region": "us-east-1"
 }
